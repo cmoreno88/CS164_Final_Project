b79272a9ec3312226582c102b167ae1eb4894f0f
cmoreno88 <cmore035@ucr.edu>
2019-12-11 22:06:13 -0800

    msg not going to correct addr

--- clientfinal.py
+++ clientfinal.py
@@ -16,6 +16,39 @@ from getpass import getpass
 # use it in this way
 # https://www.bogotobogo.com/python/Multithread/python_multithreading_subclassing_Timer_Object.php
 
+
+def usrmsg():
+	menu = {}
+	menu['1'] = "1. ryan" 
+	menu['2'] = "2. christopher"
+	menu['3'] = "3. marcus"
+	menu['4'] = "4. jacob"
+	while True: 
+	  options = menu.keys()
+	  options.sort()	# dicts are inherently not sorted so we do this here
+	  for entry in options: 
+		  print menu[entry]
+	  selection = raw_input("Please Select a User: ")
+	  if selection == '1':
+		  rcvr = "1"	# ryan
+		  return rcvr
+		  break
+	  elif selection == '2':
+		  rcvr = "2"	# christopher
+		  return rcvr
+		  break
+	  elif selection == '3':
+		  rcvr = "3"	# marcus
+		  return rcvr
+		  break
+	  elif selection == '4':
+		  rcvr = "4"	# jacob
+		  return rcvr
+		  break
+	  else:
+		  print "Please select valid user" 
+	
+
 # create CLIENT dgram udp socket
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,7 +65,9 @@ port = 8888;
 
 rsp = raw_input("Welcome to Social. Please enter your username: ")
 rflg = "8"
-reply = rflg + rsp
+tflg = "0"		# FRIEND flag
+fflg = "0"			# FROM flag
+reply = rflg + tflg + fflg + rsp
 s.sendto(reply, (host, port))
 
 while 1:
@@ -44,12 +79,14 @@ while 1:
 		data = d[0]
 		addr = d[1]
 		rflg = data[0]			# response flag
-	 	rsp = data[1:]			# response from the SERVER
+		tflg = data[1]
+		fflg = data[2]
+	 	rsp = data[3:]			# response from the SERVER
 		# print rsp
 		if rflg == "6":				# if the flag is 1 then we know that we have to ask for password		
 			print rsp
 			rsp = getpass()
-			reply = rflg + rsp
+			reply = rflg + tflg + fflg + rsp
 			# if raw_input("please press c to continue") == "c":
 			#	s.sendto(reply, (host, port))
 			s.sendto(reply, (host, port))
@@ -61,14 +98,16 @@ while 1:
 			s.sendto(reply, (host, port))
 		elif rflg == "1":
 			print'Server reply : '  + rsp
+			tflg = usrmsg() # sets friend flag
+			print tflg
 			rsp = raw_input("Enter message to send: ")
-			reply = rflg + rsp
+			reply = rflg + tflg + fflg + rsp
 			s.sendto(reply, (host, port))
 		else:
 			print rsp
 			rsp = "Goodbye From Client"
-			reply = rflg + rsp
-			if raw_input("please press c to continue") == "c":
+			reply = rflg + tflg + fflg + rsp
+			if raw_input("please press c to continue") == "c":# stops looping
 				s.sendto(reply, (host, port))
 			#s.sendto(reply, (host, port))
 		# print'Server reply : '  + rsp # data
--- serverfinal.py
+++ serverfinal.py
@@ -9,42 +9,7 @@ import sys
 from check import ip_checksum
 import time
 from getpass import getpass
-"""
-def usrmenu():
-	menu = {}
-	menu['1'] = "1. LogOut" 
-	menu['2'] = "2. Change Password"
-	# menu['3'] = "Send Message"
-	while True: 
-	  options = menu.keys()
-	  options.sort()
-	
-	  for entry in options: 
-		  print menu[entry]
-		  
-	  selection = raw_input("Please Select an Option number:")
-	  if selection == '1':
-		  print "logout" 
-		  break
-	  elif selection == '2':
-		  print "change pwd"
-	  else:
-		  print "Unknown Option Selected!" 
-	
 
-def logon():	
-	usrnm = raw_input("Welcome to Social. Please enter your username: ")
-	pwd = getpass("Please enter your PassWord: ")
-
-	print(usrnm + pwd)
-
-	if usrnm in users:
-		if users[usrnm] == pwd:
-			print("welcome")
-			usrmenu()
-		else:
-			print("no")
-"""
 HOST = ''	# Symbolic name meaning all available interfaces
 PORT = 8888	# Arbitrary non-privileged port
 # acknum = 0	# Created for acknowledgement number
@@ -52,7 +17,39 @@ users = dict({'ryan':'bently','christopher':'charlie','marcus':'shakes','jacob':
 clients = dict()	# address dict for the clients
 clientlist = []		# list to broadcast to all
 
-# Datagram (udp) socket
+def usrmsg(flg):
+	menu = {}
+	menu['1'] = "1. ryan" 
+	menu['2'] = "2. christopher"
+	menu['3'] = "3. marcus"
+	menu['4'] = "4. jacob"
+	while True: 
+	  options = menu.keys()
+	  options.sort()	# dicts are inherently not sorted so we do this here
+	  for entry in options: 
+		  print menu[entry]
+	  selection = flg # raw_input("Please Select a User: ")
+	  if selection == '1':
+		  rcvr = "ryan"
+		  return rcvr
+		  break
+	  elif selection == '2':
+		  rcvr = "christopher"
+		  return rcvr
+		  break
+	  elif selection == '3':
+		  rcvr = "marcus"
+		  return rcvr
+		  break
+	  elif selection == '4':
+		  rcvr = "jacob"
+		  return rcvr
+		  break
+	  else:
+		  print "Please select valid user" 
+
+
+# Datagram (udp) socket ####################################
 try :
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	print 'Socket created'
@@ -61,7 +58,7 @@ except socket.error, msg :
 	sys.exit()
 ##########################################################
 
-# Bind socket to local host and port
+# Bind socket to local host and port ################################
 try:
 	s.bind((HOST, PORT))
 except socket.error , msg:
@@ -78,7 +75,9 @@ while 1:
 	data = d[0]
 	addr = d[1]
 	rflg = data[0]						# reponse flag
-	rsp = data[1:]						# response from the CLIENT
+	tflg = data[1]					# friend flag
+	fflg = data[2] 						# FROM flag
+	rsp = data[3:]						# response from the CLIENT
 	# print rsp
 	if not data: 
 		print 'break'
@@ -86,29 +85,43 @@ while 1:
 	elif rsp in users.keys():
 		print 'exist'
 		curruser = rsp				# keep track of the current user
-		clients.update(rsp = addr)	# ADDS user & addr to client list
+		print curruser
+		if curruser == "ryan":
+			fflg = "1"
+		elif curruser == "christopher":
+			fflg = "2"
+		elif curruser == "marcus":
+			fflg = "3"
+		else:	# this has to be jacob
+			fflg = "4"
+		clients.update({curruser : addr})	# ADDS user & addr to client list
+		print clients
 		rflg = "6"
+		# tflg = "0"
 		rsp = "Please Enter Password: "
-		reply = rflg + rsp
+		reply = rflg + tflg + fflg + rsp
 		s.sendto(reply , addr)
 	elif rsp in users.values():
 		print 'good pass'
 		rsp = "welcome to Social!"
 		rflg = "1"		# setting the flag to 1 enters messenger
-		reply = rflg + rsp
+		reply = rflg + tflg + fflg + rsp
 		s.sendto(reply , addr)
 	elif rflg == "1":			# This option sends messages
 		# put a nested if in here to check if the message should go to 
 		# another user or be broadcast?
-		print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + rsp # data.strip()
+		fromusr = usrmsg(fflg)		# who are we sending to?
+		# print 'Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + rsp # data.strip()
+		addr = clients[fromusr]	# should set addr to send msg to
 		rsp = "Please enter message: "
-		reply = rflg + rsp
+		reply = rflg + tflg + fflg + rsp
 		s.sendto(reply , addr)
 	else:
 		print 'server else'
+		print rsp
 		rsp = "Goodbye from Server"
 		rflg = "0"
-		reply = rflg + rsp
+		reply = rflg + tflg + fflg + rsp
 		if raw_input("please press c to continue") == "c":
 			s.sendto(reply , addr)
 	# s.sendto(reply , addr)
